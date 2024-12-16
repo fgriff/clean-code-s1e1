65d7e0eeadad0d5fe002540edb2240b543f8aa22
fgriff <anglirum@gmail.com>
2024-12-17 01:45:00 +0300

    refactor: change CSS selectors according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -33,18 +33,22 @@ var createNewTaskElement=function(taskString){
   var deleteButtonImg=document.createElement("img");//delete button image
 
   label.innerText=taskString;
-  label.className="task";
+  label.className="task task-label";
+
+  listItem.className="list-item";
 
   //Each elements, needs appending
   checkBox.type="checkbox";
+  checkBox.className = "input-checkbox";
   editInput.type="text";
-  editInput.className="task";
+  editInput.className="task task-input";
 
   editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-  editButton.className="edit";
+  editButton.className="btn edit";
 
-  deleteButton.className="delete";
+  deleteButton.className="btn delete";
   deleteButtonImg.src="./remove.svg";
+  deleteButtonImg.className = "image";
   deleteButton.appendChild(deleteButtonImg);
 
 
