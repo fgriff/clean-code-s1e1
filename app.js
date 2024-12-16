59cdf0068c8c1ce7a419c15fa7c8b11183f39740
fgriff <anglirum@gmail.com>
2024-12-17 02:04:23 +0300

    fix: add alternative content for multimedia according to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ var createNewTaskElement=function(taskString){
   deleteButton.className="btn delete";
   deleteButtonImg.src="./remove.svg";
   deleteButtonImg.className = "image";
+  deleteButtonImg.setAttribute("alt", "Remove button icon");
   deleteButton.appendChild(deleteButtonImg);
 
   //and appending.
